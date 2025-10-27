5704ddb9db1ddb0c4ddd18cecff9a29f5c46eb3e
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-10-27 06:03:09 +0100

    2025-10-30 Halloween Releasse v2.25.1030

--- Deploy/console/TestCryptConsole.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-
-del README.MD.BlowFish.Fish2.Fish3.base64 README_MD.txt
-mkdir log
-EU.CqrXs.Console.Core.exe -i=.\README.MD -z=gzip  -c=BlowFish,Fish2,Fish3 -k=Hallo -e=base64 -o=.\README.MD.BlowFish.Fish2.Fish3.base64
-EU.CqrXs.Console.Core.exe -i=.\README.MD.BlowFish.Fish2.Fish3.base64  -d=base64 -D=BlowFish,Fish2,Fish3 -k=Hallo -u=gzip -o=.\README_MD.txt
-
-pause
