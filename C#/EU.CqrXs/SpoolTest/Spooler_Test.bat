6a5d012d171803e5b3a697fdaeff5bbd54f0a6b4
heinrichelsigan <heinrich.elsigan@gmail.com>
2026-01-28 17:57:30 +0100

    2026-01-28 PermAgainCrypt Console Spooler

--- C#/EU.CqrXs/SpoolTest/Spooler_Test.bat
+++ C#/EU.CqrXs/SpoolTest/Spooler_Test.bat
@@ -2,6 +2,8 @@
 
 echo "EU.CqrXs.SpoolTest.exe -i=S:\PermAgainCrypt\Deploy\SpoolTest\In -o=S:\PermAgainCrypt\Deploy\SpoolTest\Out -k=S:\PermAgainCrypt\Deploy\SpoolTest\In\keys.txt"
 
-EU.CqrXs.SpoolTest.exe -i=S:\PermAgainCrypt\Deploy\SpoolTest\In -o=S:\PermAgainCrypt\Deploy\SpoolTest\Out -k=S:\PermAgainCrypt\Deploy\SpoolTest\In\keys.txt
+EU.CqrXs.SpoolTest.exe -V -S -i=S:\PermAgainCrypt\Deploy\SpoolTest\In -o=S:\PermAgainCrypt\Deploy\SpoolTest\Encrypt -k=S:\PermAgainCrypt\Deploy\SpoolTest\In\keys.txt
+echo "Decompressing from directory!"
 
+EU.CqrXs.SpoolTest.exe -V -D -S -i=S:\PermAgainCrypt\Deploy\SpoolTest\Encrypt -o=S:\PermAgainCrypt\Deploy\SpoolTest\Out -k=S:\PermAgainCrypt\Deploy\SpoolTest\In\keys.txt
 pause
