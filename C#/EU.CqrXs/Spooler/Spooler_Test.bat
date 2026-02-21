450c04233071213e8afbca5b95ec7876720e5580
heinrichelsigan <heinrich.elsigan@gmail.com>
2026-02-21 02:05:57 +0100

    2026-02-21 -s | --SecureCipherPipe release v2.26.221 performs an en-/decryption via SecureCipherPipe in Console & Spooler via option -S

--- C#/EU.CqrXs/Spooler/Spooler_Test.bat
+++ C#/EU.CqrXs/Spooler/Spooler_Test.bat
@@ -1,20 +1,29 @@
 @echo off 
 
+echo "deleting old files in Encrypt\* Out\*"
+del /s /q .\Encrypt\*.*
+del /s /q .\Out\*.*
+
+:initial
+if "%1"=="-S" goto secpipe
+if "%1"=="-s" goto secpipe
+
 echo "EU.CqrXs.Spooler.exe -V -k=bar@ba.area23.at -i=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\In\ -o=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Encrypt\
 EU.CqrXs.Spooler.exe -V -k=bar@ba.area23.at -i=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\In\ -o=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Encrypt\ 
 echo "EU.CqrXs.Spooler.exe -V -D -i=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Encrypt\ -o=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Out\ "
 EU.CqrXs.Spooler.exe -V -D -k=bar@ba.area23.at -i=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Encrypt\ -o=S:\PermAgainCrypt\Deploy\EU.CqrXs\EU.CqrXs.Spooler\Out\ 
-
-del /s /v /q Encrypt\*.*
-pause
-del /s /v /q Out\*.*
+goto end
 
 
+:secpipe
 EU.CqrXs.Spooler.exe -V -S -k=jo@io.cqrxs.eu -i=.\In\ -o=.\Encrypt\ 
 echo "Decompressing from directory!"
 EU.CqrXs.Spooler.exe -D -V -S -k=jo@io.cqrxs.eu -i=.\Encrypt\ -o=.\Out\ 
-del /s /v /q Encrypt\*.*
-pause
-del /s /v /q Out\*.*
+goto end
+
+
+:end
+
+
 
 REM exit
